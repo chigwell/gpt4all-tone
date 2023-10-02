400bd20db0eeb3c8e5093180679b19d02f65af3b
E. Evstafiev <ee345@cam.ac.uk>
2023-10-02 20:17:22 +0100

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gpt4all-tone',
-    version='0.2',
+    version='0.23',
     packages=find_packages(),
     install_requires=[
         "argparse",
